ca31e12bada408fe811b6eec862ef05eeaf9277b
rohmanalka <rohmanalka06@gmail.com>
2025-10-11 22:25:17 +0700

    feat: add contact section with formsubmit.co

--- src/App.jsx
+++ src/App.jsx
@@ -145,6 +145,32 @@ function App() {
           </div>
         </div>
         {/* Serti Section */}
+
+        {/* Contact Section */}
+        <div className="kontak mt-32 sm:p-10 p-0" id="kontak">
+          <h1 className="text-4xl mb-2 font-bold text-center" data-aos="zoom-in" data-aos-duration="1000" data-aos-once="true">Kontak</h1>
+          <p className="text-base/loose text-center mb-10 opacity-50" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">Mari Terhubung dengan Saya</p>
+          <form action="https://formsubmit.co/rohmanalka06@gmail.com" method="POST" className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md" autoComplete="off" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500" data-aos-once="true">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold">Nama Lengkap</label>
+                <input type="text" name="nama" placeholder="Masukkan Nama..." className="border border-zinc-500 p-2 rounded-md" required />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold">Email</label>
+                <input type="email" name="email" placeholder="Masukkan Email..." className="border border-zinc-500 p-2 rounded-md" required />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="pesan" className="font-semibold">Pesan</label>
+                <textarea name="pesan" id="pesan" cols="45" rows="7" placeholder="Pesan..." className="border border-zinc-500 p-2 rounded-md" required></textarea>
+              </div>
+              <div className="text-center">
+                <button type="submit" className="bg-violet-700 p-3 rounded-lg w-full cursor-pointer border border-zinc-600 hover:bg-violet-600 cursor-target">Kirim Pesan</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        {/* Contact Section */}
     </>
   )
 }
