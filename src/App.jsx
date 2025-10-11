56da256eb06b0c3fb0ef9edc08b94433450915e4
rohmanalka <rohmanalka06@gmail.com>
2025-10-11 22:17:55 +0700

    feat: add project section

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
               </div>
             </div>
           </div>
-          
+
           <div className="tools mt-32">
             <h1 className="text-4xl/snug font-bold mb-4" data-aos="zoom-in" data-aos-duration="1000" data-aos-once="true">Tools yang dipakai</h1>
             <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">
@@ -85,6 +85,34 @@ function App() {
           </div>
         </div>
         {/* About Section */}
+
+        {/* Proyek Section */}
+        <div className="proyek mt-32 py-10" id="proyek">
+          <h1 className="text-center text-4xl font-bold mb-2" data-aos="zoom-in" data-aos-duration="1000" data-aos-once="true">Proyek</h1>
+          <p className="text-base/loose text-center opacity-50" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">
+            Berikut beberapa proyek yang telah saya buat
+          </p>
+          <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+            {listProyek.map(proyek => (
+              <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay={proyek.dad} data-aos-once="true">
+                <img src={proyek.gambar} alt="Proyek Image" loading="lazy" />
+                <div>
+                  <h1 className="text-2xl font-bold my-4">{proyek.nama}</h1>
+                  <p className="text-base/loose mb-4">{proyek.desk}</p>
+                  <div className="flex flex-wrap gap-2">
+                    {proyek.tools.map((tool, index) => (
+                      <p className="py-1 px-3 border border-zinc-500 bg-zinc-600 rounded-md font-semibold" key={index}>{tool}</p>
+                    ))}
+                  </div>
+                  <div className="mt-8 text-center">
+                    <a href={proyek.link} className="bg-violet-700 p-3 rounded-lg block border border-zinc-600 hover:bg-violet-600 cursor-target">Dokumentasi</a>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* Proyek Section */}
     </>
   )
 }
