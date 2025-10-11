59a0b98d1ac4d553ff621db5cc59a67bc646e2b7
rohmanalka <rohmanalka06@gmail.com>
2025-10-11 22:16:14 +0700

    feat: add tech section

--- src/App.jsx
+++ src/App.jsx
@@ -64,6 +64,25 @@ function App() {
               </div>
             </div>
           </div>
+          
+          <div className="tools mt-32">
+            <h1 className="text-4xl/snug font-bold mb-4" data-aos="zoom-in" data-aos-duration="1000" data-aos-once="true">Tools yang dipakai</h1>
+            <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">
+              Berikut beberapa tools yang biasa saya pakai
+            </p>
+            <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+              {listTools.map(tool => (
+                <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md 
+                hover:bg-zinc-800 group cursor-target" key={tool.id} data-aos="zoom-in" data-aos-duration="1000" data-aos-delay={tool.dad} data-aos-once="true">
+                <img src={tool.gambar} alt="Tools Image" className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900" />
+                <div>
+                  <h4 className="font-bold">{tool.nama}</h4>
+                  <p className="opacity-50">{tool.keterangan}</p>
+                </div>
+              </div>
+              ))}
+            </div>
+          </div>
         </div>
         {/* About Section */}
     </>
